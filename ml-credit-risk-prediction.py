c15451f1e780386fbf91b163ad0c938710dd3de4
KOCCENG <mehmetkoc2229@gmail.com>
2026-01-29 20:47:17 +0300

    Merge düzeltmesi yapıldı

---
+++ ml-credit-risk-prediction.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+df = pd.read_csv("data/credit_card_default_taiwan.csv", sep=";")
+df.head()
+
+
+# In[ ]:
+
+
+####Regression
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+####Random-Forest
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
